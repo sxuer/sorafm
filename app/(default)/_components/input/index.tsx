144fa7ff17da7c56c7ca5e262f7bd4db61adc9b4
devidai <devidai@tencent.com>
2024-02-23 13:39:37 +0800

    1

--- app/(default)/_components/input/index.tsx
+++ app/(default)/_components/input/index.tsx
@@ -52,7 +52,7 @@ export default function () {
         type="text"
         required
         className="min-w-0 flex-auto rounded-md border-0 bg-white/5 px-3.5 py-2 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-white sm:text-sm sm:leading-6"
-        placeholder="目前Sora能力尚未开放，请耐心等待"
+        placeholder="sora尚未开放，请耐心等待"
         value={description}
         onChange={(e) => setDescription(e.target.value)}
         onKeyDown={handleInputKeydown}
